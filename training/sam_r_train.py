176f25aadf4e08a34cea89c918b442ee291b5216
Samuel Rae <samuelrae@Samuels-Air.fritz.box>
2024-11-28 11:13:18 +0000

    fix training loop

--- training/sam_r_train.py
+++ training/sam_r_train.py
@@ -18,32 +18,21 @@ dataloader = DataLoader(dataset, batch_size=1)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 criterion = torch.nn.CrossEntropyLoss()
 
-
-def validate(model, audio):
-    model.eval()
-    torch.set_grad_enabled(False)
-    
-    inputs = processor(audio.squeeze(0), sampling_rate=16000, return_tensors="pt", return_attention_mask=True)
-    generated_ids = model.generate(
-        inputs["input_features"],
-        attention_mask=inputs["attention_mask"]
-    )
-    transcription = processor.batch_decode(generated_ids, skip_special_tokens=False)
-    print(f"final_prediction from generate: {transcription}")
+notimestamps_token_id = tokenizer.convert_tokens_to_ids('<|notimestamps|>')
 
 def validate_sam(model, input_mel):
     model.eval()
-    torch.set_grad_enabled(False)
-    input_tkns = get_start_input_ids(tokenizer)
-    input_tkns_tensor = get_input_tensor(input_tkns, model.device)
+    with torch.no_grad():
+        input_tkns = get_start_input_ids(tokenizer)
+        input_tkns_tensor = get_input_tensor(input_tkns, model.device)
 
-    for i in range(80):
-        initial_predictions = model(decoder_input_ids=input_tkns_tensor, input_features=input_mel)
-        # 
-        next_tkn = torch.argmax(initial_predictions.logits, dim=-1)[0,-1].unsqueeze(0)
-        input_tkns_tensor = torch.cat((input_tkns_tensor.squeeze(), next_tkn), dim=0).unsqueeze(0)
-        if input_tkns_tensor[-1, -1].item() == eot_token_id:
-            break
+        for i in range(80):
+            initial_predictions = model(decoder_input_ids=input_tkns_tensor, input_features=input_mel)
+            # 
+            next_tkn = torch.argmax(initial_predictions.logits, dim=-1)[0,-1].unsqueeze(0)
+            input_tkns_tensor = torch.cat((input_tkns_tensor.squeeze(), next_tkn), dim=0).unsqueeze(0)
+            if input_tkns_tensor[-1, -1].item() == eot_token_id:
+                break
 
     decoded_initial_output = tokenizer.decode(input_tkns_tensor.squeeze().tolist())
     print("===================")
@@ -51,27 +40,50 @@ def validate_sam(model, input_mel):
     print("===================")
 
 def train(model, input_mel, input_tensor, target_tensor):
-    torch.set_grad_enabled(True)
     model.train()
+    total_loss = 0
+    for name,param in model.named_parameters():
+        if(not param.requires_grad):
+            print(f"Parameter {name} has requires_grad=False")
+    
     for step in range(10):
-        predictions = model(decoder_input_ids=input_tensor, input_features=input_mel).logits
-        loss = criterion(predictions.transpose(1, 2), target_tensor)
         optimizer.zero_grad()
+        
+        outputs = model(
+            decoder_input_ids=input_tensor, 
+            input_features=input_mel
+        )
+        
+        # TODO shift predictions here rather than before
+        # shift_logits = outputs.logits[..., :-1, :].contiguous()
+        # shift_targets = target_tensor[..., 1:].contiguous()
+        # print(f"len: {len(model.parameters())}")
+        
+        loss = criterion(outputs.logits.transpose(1, 2), target_tensor)
         loss.backward()
+        
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)  # Add gradient clipping
         optimizer.step()
+        
+        total_loss += loss.item()
         print(f"Step {step}, Loss: {loss.item()}")
+
 
 
 for batch in dataloader:
     # squeeze to remove channel dimension
     audio = batch['audio'].squeeze(1)
     # TODO is there a transformers method for this?
-    input_mel = whisper.log_mel_spectrogram(audio)
+    input_mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
     input_tkns_batched = [get_target_input_ids(transcript, tokenizer) for transcript in batch['transcript']]
-    target_tkns_batched = [batch[1:] + [eot_token_id] for batch in input_tkns_batched]
     input_tkns_tensor = get_input_tensor(input_tkns_batched, model.device).squeeze(1)
-    target_tkns_tensor = get_input_tensor(input_tkns_tensor, model.device).squeeze(1)
+    target_tkns_tensor = get_input_tensor(input_tkns_batched, model.device).squeeze(1)
+
+    # shifting for teacher forcing
+    input_tkns_tensor = input_tkns_tensor[:, :-1].contiguous()
+    target_tkns_tensor = target_tkns_tensor[:, 1:].contiguous()
+
 
     train(model, input_mel, input_tkns_tensor, target_tkns_tensor)
     validate_sam(model, input_mel)

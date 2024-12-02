c76a3621e7c27624204b48403a9f6dac0eb41555
Samuel Rae <samuelrae@Samuels-Air.fritz.box>
2024-12-02 16:46:17 +0000

    fix: validation script

--- training/validate_generalisation.py
+++ training/validate_generalisation.py
@@ -10,8 +10,9 @@ from training.sam_r_train import validate_batch
 from training.utils import collate_fn
 
 def main():
-    model = CustomWhisper(base_model="openai/whisper-tiny", max_speakers=5)
-    model.load_state_dict(torch.load("./weights/whisper_diarization_weights.pth", weights_only=True))
+    custom_model_wrapper = CustomWhisper(base_model="openai/whisper-tiny", max_speakers=5)
+    model = custom_model_wrapper.model
+    model.load_state_dict(torch.load("./weights/whisper_diarization_v3.pth", weights_only=True))
     dataset = HomegrownDataset(split='validate')
     dataloader = DataLoader(dataset, batch_size=2, collate_fn=collate_fn)
     for batch in dataloader:

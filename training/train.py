a7cf8aa21a53acb1d3cf012c247cae35debaf710
Samuel Rae <samuelrae@Samuels-Air.fritz.box>
2024-12-05 13:34:53 +0000

    increase amt of training

--- training/train.py
+++ training/train.py
@@ -104,10 +104,10 @@ def main():
     print("datasets and dataloaders")
     # train_dataset = HomegrownDataset(split='train', numbered_speakers=numbered_speakers)
     # TODO better design isto pass tokenizer into Ami
-    train_dataset = Ami(split="train", subset_size=5000)
+    train_dataset = Ami(split="train")
     # because the actual length of the dataset is unpredictable ( it depends on how the conversations get chunked up) we need drop_last=True or there
     # may be mismatch and the dataloader will try to iterate too many time
-    train_dataloader = DataLoader(train_dataset, batch_size=64, collate_fn=Ami.get_collate_fn(train_dataset.tk, train_dataset.extractor))
+    train_dataloader = DataLoader(train_dataset, batch_size=128, collate_fn=Ami.get_collate_fn(train_dataset.tk, train_dataset.extractor))
 
     # val_dataset = HomegrownDataset(split='validate', numbered_speakers=numbered_speakers)
     # val_dataset = Ami(split="validation", subset_size=500)
